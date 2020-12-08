f9a70f57e9427c38e66e0419fff1b77e0763a4fb
guillemdb <guillem@fragile.tech>
2020-12-08 11:55:10 +0100

    Disable lint for docs

--- mltemplate/__main__.py
+++ mltemplate/__main__.py
@@ -1,6 +1,6 @@
 import os
-from pathlib import Path
 import sys
+from pathlib import Path
 
 from mltemplate.ci.writers import write_ci_config
 from mltemplate.directories import setup_project_project_files
