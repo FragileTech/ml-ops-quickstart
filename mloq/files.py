f4a5a6a7217bb15261c2ed911b62af4c17829a1e
guillemdb <guillem@fragile.tech>
2020-12-14 15:45:45 +0100

    Add different workflows. Improve file templating

--- mloq/files.py
+++ mloq/files.py
@@ -2,7 +2,14 @@ from collections import namedtuple
 from pathlib import Path
 
 
-File = namedtuple("File", "name src is_static")
+File = namedtuple("File", "name src dst is_static")
+
+
+def file(name, path, dst=None, is_static: bool = False):
+    dst = dst if dst is not None else name
+    return File(name=name, src=path / name, dst=dst, is_static=is_static)
+
+
 # Assets paths
 ASSETS_PATH = Path(__file__).parent / "assets"
 TEMPLATES_PATH = ASSETS_PATH / "templates"
@@ -10,59 +17,41 @@ STATIC_FILES_PATH = ASSETS_PATH / "static"
 REQUIREMENTS_PATH = ASSETS_PATH / "requirements"
 WORKFLOWS_PATH = ASSETS_PATH / "workflows"
 
-# Static files
-repository = File(name="repository.yml", src=STATIC_FILES_PATH / "repository.yml", is_static=True)
-gitignore = File(name=".gitignore", src=STATIC_FILES_PATH / ".gitignore", is_static=True)
-dco = File(name="DCO.md", src=STATIC_FILES_PATH / "DCO.md", is_static=True)
-init = File(name="__init__.py", src=STATIC_FILES_PATH / "__init__.py", is_static=True)
-version = File(name="version.py", src=STATIC_FILES_PATH / "version.py", is_static=True)
-main = File(name="__main__.py", src=STATIC_FILES_PATH / "__main__.py", is_static=True)
-code_of_conduct = File(
-    name="code_of_conduct.md", src=TEMPLATES_PATH / "code_of_conduct.md", is_static=False
-)
+# Common files
+repository = file("repository.yml", STATIC_FILES_PATH, is_static=True)
+gitignore = file(".gitignore", STATIC_FILES_PATH, is_static=True)
+dco = file("DCO.md", STATIC_FILES_PATH, is_static=True)
+init = file("__init__.py", STATIC_FILES_PATH, is_static=True)
+main = file("__main__.py", STATIC_FILES_PATH, is_static=True)
+version = file("version.py", STATIC_FILES_PATH, is_static=True)
+code_of_conduct = file("code_of_conduct.md", TEMPLATES_PATH)
 
 # Requirements files
-data_science_req = File(
-    name="data-science.txt", src=REQUIREMENTS_PATH / "data-science.txt", is_static=True
-)
-data_viz_req = File(
-    name="data-visualization.txt",
-    src=REQUIREMENTS_PATH / "data-visualization.txt",
-    is_static=True,
-)
-pytorch_req = File(name="pytorch.txt", src=REQUIREMENTS_PATH / "pytorch.txt", is_static=True)
-tensorflow_req = File(
-    name="tensorflow.txt", src=REQUIREMENTS_PATH / "tensorflow.txt", is_static=True
-)
-lint_req = File(
-    name="requirements-lint.txt", src=REQUIREMENTS_PATH / "requirements-lint.txt", is_static=True,
-)
-test_req = File(
-    name="requirements-test.txt", src=REQUIREMENTS_PATH / "requirements-test.txt", is_static=True,
-)
+data_science_req = file("data-science.txt", REQUIREMENTS_PATH)
+data_viz_req = file("data-visualization.txt", REQUIREMENTS_PATH, is_static=True)
+pytorch_req = file("pytorch.txt", REQUIREMENTS_PATH, is_static=True)
+tensorflow_req = file("tensorflow.txt", REQUIREMENTS_PATH, is_static=True)
+lint_req = file("requirements-lint.txt", REQUIREMENTS_PATH, is_static=True)
+test_req = file("requirements-test.txt", REQUIREMENTS_PATH, is_static=True)
 
 # Templates
-dockerfile = File(name="Dockerfile", src=TEMPLATES_PATH / "Dockerfile", is_static=False)
-mit_license = File(name="LICENSE", src=TEMPLATES_PATH / "LICENSE", is_static=False)
-makefile = File(name="Makefile", src=TEMPLATES_PATH / "Makefile", is_static=False)
-mlproject = File(name="MLproject", src=TEMPLATES_PATH / "MLproject", is_static=False)
-readme = File(name="README.md", src=TEMPLATES_PATH / "README.md", is_static=False)
-setup_py = File(name="setup.py", src=TEMPLATES_PATH / "setup.py", is_static=False)
-pyproject_toml = File(
-    name="pyproject.toml", src=TEMPLATES_PATH / "pyproject.toml", is_static=False
-)
+mit_license = file("MIT_LICENSE", TEMPLATES_PATH, "LICENSE")
+setup_py = file("setup.py", TEMPLATES_PATH)
+dockerfile = file("Dockerfile", TEMPLATES_PATH)
+makefile = file("Makefile", TEMPLATES_PATH)
+mlproject = file("MLproject", TEMPLATES_PATH)
+readme = file("README.md", TEMPLATES_PATH)
+pyproject_toml = file("pyproject.toml", TEMPLATES_PATH)
 
 # Workflows
-bump_version_wkf = File(
-    name="bump-version.yml", src=WORKFLOWS_PATH / "bump-version.yml", is_static=False
-)
-lint_test_wkf = File(
-    name="lint-and-pytest.yml", src=WORKFLOWS_PATH / "lint-and-pytest.yml", is_static=False
-)
-docker_test_wkf = File(
-    name="test-docker.yml", src=WORKFLOWS_PATH / "test-docker.yml", is_static=False
-)
-
+bump_version_wkf = file("bump-version.yml", WORKFLOWS_PATH)
+lint_test_wkf = file("lint-and-pytest.yml", WORKFLOWS_PATH)
+docker_test_wkf = file("test-docker.yml", WORKFLOWS_PATH)
+build_pypitest_python_wkf = file("build-pure-python.yml", WORKFLOWS_PATH, "build-testpypi.yml")
+build_pypitest_source_wkf = file("build-source.yml", WORKFLOWS_PATH, "build-source-testpypi.yml")
+build_pypitest_wheels_wkf = file("build-wheels.yml", WORKFLOWS_PATH, "build-wheels-testpypi.yml")
+release_package_wkf = file("release-package.yml", WORKFLOWS_PATH, "release-package.yml")
+release_wheels_wkf = file("release-platform-wheels.yml", WORKFLOWS_PATH, "release-package.yml")
 
 ROOT_PATH_FILES = [
     dco,
