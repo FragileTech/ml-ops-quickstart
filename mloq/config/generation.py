f61116598e247f4f1f4787c79b09a67e3c65985d
Vadim Markovtsev <vadim@athenian.co>
2021-02-04 20:31:51 +0100

    Refactor parameters definition
    Signed-off-by: Vadim Markovtsev <vadim@athenian.co>

--- mloq/config/generation.py
+++ mloq/config/generation.py
@@ -2,7 +2,7 @@
 from typing import Optional
 
 from mloq.config.logic import get_docker_image, load_empty_config
-from mloq.config.params import Config, PROJECT_CONFIG, TEMPLATE
+from mloq.config.params import Config, PROJECT, TEMPLATE
 
 
 def generate_project_config(
@@ -28,16 +28,16 @@ def generate_project_config(
     project_config = load_empty_config()["project"]
     project_config.update(_project)
     # Fill in project values
-    project_config["open_source"] = PROJECT_CONFIG["open_source"](
+    project_config["open_source"] = PROJECT.open_source(
         project_config,
         interactive,
         default=True,
     )
-    # use_docker = PROJECT_CONFIG["docker"](project_config, interactive, default=True)
+    # use_docker = PROJECT.docker(project_config, interactive, default=True)
     project_config["docker"] = True  # use_docker
-    project_config["ci"] = PROJECT_CONFIG["ci"](project_config, interactive, default="python")
-    project_config["mlflow"] = PROJECT_CONFIG["mlflow"](project_config, interactive, default=False)
-    project_config["requirements"] = PROJECT_CONFIG["requirements"](
+    project_config["ci"] = PROJECT.ci(project_config, interactive, default="python")
+    project_config["mlflow"] = PROJECT.mlflow(project_config, interactive, default=False)
+    project_config["requirements"] = PROJECT.requirements(
         project_config,
         interactive,
         default="None",
@@ -73,34 +73,34 @@ def generate_template(
     project_config.update(_project)
     template.update(_template)
     # Fill in template values
-    template["project_name"] = TEMPLATE["project_name"](template, interactive)
-    template["description"] = TEMPLATE["description"](template, interactive)
-    template["default_branch"] = TEMPLATE["default_branch"](
+    template["project_name"] = TEMPLATE.project_name(template, interactive)
+    template["description"] = TEMPLATE.description(template, interactive)
+    template["default_branch"] = TEMPLATE.default_branch(
         template,
         interactive,
         default="master",
     )
-    template["owner"] = TEMPLATE["owner"](template, interactive)
-    template["author"] = TEMPLATE["author"](template, interactive, default=template["owner"])
-    template["email"] = TEMPLATE["email"](template, interactive)
-    copyright_holder = TEMPLATE["copyright_holder"](
+    template["owner"] = TEMPLATE.owner(template, interactive)
+    template["author"] = TEMPLATE.author(template, interactive, default=template["owner"])
+    template["email"] = TEMPLATE.email(template, interactive)
+    copyright_holder = TEMPLATE.copyright_holder(
         template,
         interactive,
         default=template["owner"],
     )
     template["copyright_holder"] = copyright_holder
     default_url = f"https://github.com/{template['owner']}/{template['project_name']}"
-    template["project_url"] = TEMPLATE["project_url"](template, interactive, default=default_url)
-    versions = TEMPLATE["python_versions"](template, interactive, default="3.6, 3.7, 3.8, 3.9")
+    template["project_url"] = TEMPLATE.project_url(template, interactive, default=default_url)
+    versions = TEMPLATE.python_versions(template, interactive, default="3.6, 3.7, 3.8, 3.9")
     template["python_versions"] = versions
     # If project is undefined parse the full template
     if project_config["ci"] or project_config["ci"] is None:
-        template["bot_name"] = TEMPLATE["bot_name"](
+        template["bot_name"] = TEMPLATE.bot_name(
             template,
             interactive,
             default=template["author"],
         )
-        template["bot_email"] = TEMPLATE["bot_email"](
+        template["bot_email"] = TEMPLATE.bot_email(
             template,
             interactive,
             default=template["email"],
@@ -108,9 +108,9 @@ def generate_template(
     if not project_config["open_source"] or project_config["open_source"] is None:
         template["license"] = "proprietary"
     else:
-        template["license"] = TEMPLATE["license_type"](template, interactive, default="MIT")
+        template["license"] = TEMPLATE.license_type(template, interactive, default="MIT")
     base_docker = get_docker_image(template=template, project_config=project_config)
     if base_docker is not None:
-        base_docker = TEMPLATE["docker_image"](template, interactive, default=base_docker)
+        base_docker = TEMPLATE.docker_image(template, interactive, default=base_docker)
     template["docker_image"] = str(base_docker) if base_docker is None else base_docker
     return template
